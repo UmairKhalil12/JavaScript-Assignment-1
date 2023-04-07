0d7fd31b53f02ec5ac25c1c2afbe53d1d24b0fbe
nooblearner12 <khalilumair21@gmail.com>
2023-04-07 15:27:59 +0500

    Assignment

---
+++ index.js
@@ -0,0 +1,133 @@
+// -------------------------------- QUESTION 1 ------------------------------ 
+
+// var num1 = +prompt("Enter number 1");
+// var num2 = +prompt("Enter number 2");
+
+// if (num1 > num2){
+//     console.log( num1 + " is greater" );
+// }
+
+// else if (num1 < num2){
+//     console.log( num2 + " is greater");
+// }
+
+// ---------------------------------QUESTION 2 --------------------------------
+
+// var number = +prompt("Enter number either negative or postive");
+// if (number < 0){
+//     alert(" Question 2: The sign is -" );
+// } 
+// else if (number > 0){
+//     alert("Question 2: The sign is +");
+// }
+
+// ---------------------------------QUESTION 3 --------------------------------
+
+// var num1 = +prompt("Enter first number");
+// var num2 = +prompt("Enter second number");
+// var num3 = +prompt("Enter third number");
+// var num4 = +prompt("Enter fourth number");
+// var num5 = +prompt("Enter fifth number");
+
+
+// if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5  ){
+//     console.log( num1 + " is greater");
+// }
+
+// else if (num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5){
+//     console.log(num2 + " is greater");
+// }
+
+// else if (num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5){
+//     console.log(num3 + " is greater");
+// }
+
+// else if (num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5){
+//     console.log( num4  +  " is greater");
+// }
+
+// else if (num5 > num1 && num5 > num2 && num5 > num3 && num5 > num4){
+//     console.log(num5 +  " is greater");
+// }
+
+// ---------------------------------QUESTION 4 --------------------------------
+
+// for (let i = 0; i <= 15; i++) {
+//     if (i % 2 === 0){
+//         document.write("<br>");
+//         document.write(" number " + i + " is even ");
+//     }
+//     else{
+//         document.write("<br>")
+//         document.write(" number "+ i + " is odd ");
+//     }
+// }
+
+// ---------------------------------QUESTION 5 --------------------------------
+
+// var sub1 = +prompt("Enter subject 1 marks");
+// var sub2 = +prompt("Enter subject 2 marks");
+// var sub3 = +prompt("Enter subject 3 marks");
+// var sub4 = +prompt("Enter subject 4 marks");
+// var sub5 = +prompt("Enter subject 5 marks");
+
+// var average = (sub1 + sub2 + sub3 + sub4 + sub5)/5 ; 
+
+// console.log("Your average marks are: " + average);
+
+// if (average < 60){
+//     console.log("Fail");
+// }
+
+// else if (average < 70){
+//     console.log("D grade");
+// }
+
+// else if (average < 80){
+//     console.log("C grade");
+// }
+
+// else if (average < 90){
+//     console.log("B grade");
+// }
+
+// else if (average < 100){
+//     console.log("A grade");
+// }
+
+// ---------------------------------QUESTION  6 --------------------------------
+
+// for(let i = 1; i <= 100; i++ ){
+
+//     if ( i % 3 === 0){
+//         console.log(i,"Fizz")
+//     }
+
+//     if ( i % 5 === 0){
+//         console.log(i, "Buzz")
+//     }
+
+//     if ( i % 3 === 0 && i % 5 === 0){
+//         console.log(i, "FizzBuzz")
+//     }
+
+// }
+
+// ---------------------------------QUESTION  7 --------------------------------
+
+// var stars = " ";
+// for (let i = 1; i <= 5; i ++){
+//     for(let j= 0; j < i; j++){
+//         stars += "*";
+       
+//     }
+//     stars+= "\n ";
+// }
+
+// console.log(stars);
+
+
+
+
+
+
